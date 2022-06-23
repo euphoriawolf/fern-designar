626d69a1e2beecf25c491863809090f3e449238c
Chukwuemeka John Okparaeke <41091907+mactrix-markjohn@users.noreply.github.com>
2022-06-23 11:36:23 +0100

    Firebase integration

--- Assets/Scripts/Auth/Auth.cs
+++ Assets/Scripts/Auth/Auth.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Firebase.Auth;
+using TMPro;
 using UnityEngine;
 
 public class Auth : MonoBehaviour
@@ -10,11 +12,23 @@ public class Auth : MonoBehaviour
     [SerializeField] private GameObject signUpPage;
     [SerializeField] private GameObject SignUpSuccessPanel;
     
+    [SerializeField] private TMP_InputField emailSignIn, passwordSignIn;
+    [SerializeField] private TMP_InputField firstnameSignUp, lastnameSignUp, emailSignUp, passwordSignUp , comfirmpasswordSignUp;
+    
+    private FirebaseAuth auth;
+    private FirebaseUser userAuth;
+    
+    
+    private string deviceID;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
         signUpPage.SetActive(false);
         SignUpSuccessPanel.SetActive(false);
+        
+        auth = FirebaseAuth.DefaultInstance;
     }
 
     // Update is called once per frame
@@ -68,22 +82,97 @@ public class Auth : MonoBehaviour
     }
 
 
+
+    // --- Sign In methods and instructions ----------
+    
+    
     public void SignInAuthClick()
     {
         // after signing in
-        GetComponent<SceneTransition>().TransitionScene(StringManifest.Scene_Homepage);
+        SignInFunc();
+        
+        
+    }
+    
+    
+    
+    public void SignInFunc()
+    {
+
+       // WelcomeScript._ShowAndroidToastMessage("SignIn clicked");
+
+        var _emailtext = emailSignIn.text;
+        var _passwordtext = passwordSignIn.text;
+        
+        if (string.IsNullOrEmpty(_emailtext) && string.IsNullOrEmpty(_passwordtext))
+        {
+            //WelcomeScript._ShowAndroidToastMessage("Please fill the Email and Password entry");
+        }
+        else
+        {
+            //WelcomeScript._ShowAndroidToastMessage("This are the entered details: "+ _emailtext+" "+_passwordtext);
+            StartCoroutine(Login(_emailtext, _passwordtext));
+
+            
+        }
+
+        
     }
 
+    private IEnumerator Login(string email, string password)
+    {
+        //var auth = FirebaseAuth.DefaultInstance;
+        var registerTask = auth.SignInWithEmailAndPasswordAsync(email, password);
+        
+        yield return new WaitUntil(() => registerTask.IsCompleted);
+
+        if (registerTask.Exception != null)
+        {
+            Debug.LogWarning($"Failed to Login user with {registerTask.Exception}");
+          
+            // WelcomeScript._ShowAndroidToastMessage($"Failed to Login user with {registerTask.Exception}");
+        }
+        else
+        {
+            
+            Debug.LogWarning($"Successfully logged in user {registerTask.Result.Email}");
+            
+            GetComponent<SceneTransition>().TransitionScene(StringManifest.Scene_Homepage);
+
+            //WelcomeScript._ShowAndroidToastMessage($"Successfully logged in User {registerTask.Result.Email}");
+            //SceneManager.LoadScene(StringHolder.Scene_ExperiencesScene);
+        }
+        
+
+
+
+    }
+
+    public void SignInMakePasswordVisible()
+    {
+        
+    }
+
+
+
+    // --------------- End ------------------------ 
+
+    
+    
+    
+    
+    
+    
+    
+    
+    // -------- Sign Up methods and instructions ------------
+    
     public void SignUpAuthClick()
     {
         //after signing up
         
-        SignUpSuccessPanel.SetActive(true);
-
-        SignUpSuccessPanel.GetComponent<CanvasGroup>().LeanAlpha(1, 0.5f);
-        
-        signInPage.SetActive(false);
-        signUpPage.SetActive(false);
+        SignUpFunc();
+       
     }
 
     public void SignUpContinueClick()
@@ -93,4 +182,84 @@ public class Auth : MonoBehaviour
         
         GetComponent<SceneTransition>().TransitionScene(StringManifest.Scene_Homepage);
     }
+
+
+    public void SignUpFunc()
+    {
+        //WelcomeScript._ShowAndroidToastMessage("SignUp pressed");
+
+        string name = "Mark John";
+        
+        if (string.IsNullOrEmpty(name) && string.IsNullOrEmpty(emailSignUp.text) && string.IsNullOrEmpty(passwordSignUp.text))
+        {
+            //WelcomeScript._ShowAndroidToastMessage("Please fill in the missing entries");
+        }
+        else
+        {
+            if (passwordSignUp.text.Equals(comfirmpasswordSignUp.text))
+            {
+                StartCoroutine(Register(emailSignUp.text, passwordSignUp.text));
+
+            }
+            else
+            {
+                print("Password not the same");
+            }
+            //WelcomeScript._ShowAndroidToastMessage("Email entered:  " + emailSignUp.text);
+
+            
+        }
+        
+    }
+
+
+    private IEnumerator Register(string email, string password)
+    {
+        //var auth = FirebaseAuth.DefaultInstance;
+        var registerTask = auth.CreateUserWithEmailAndPasswordAsync(email, password);
+        
+        yield return new WaitUntil(() => registerTask.IsCompleted);
+
+        if (registerTask.Exception != null)
+        {
+            Debug.LogWarning($"Failed to register user with {registerTask.Exception}");
+            //WelcomeScript._ShowAndroidToastMessage($"Failed to register user with {registerTask.Exception}");
+        }
+        else
+        {
+            Debug.LogWarning($"Successfully Registered user {registerTask.Result.Email}");
+            //WelcomeScript._ShowAndroidToastMessage($"Successfully Registered user {registerTask.Result.Email}");
+            
+            userAuth = registerTask.Result;
+            
+            //SaveUsername();
+            
+            OpenSignUpSuccessPanel();
+
+        }
+
+    }
+
+    void OpenSignUpSuccessPanel()
+    {
+        
+        SignUpSuccessPanel.SetActive(true);
+
+        SignUpSuccessPanel.GetComponent<CanvasGroup>().LeanAlpha(1, 0.5f);
+        
+        signInPage.SetActive(false);
+        signUpPage.SetActive(false); 
+    }
+    
+    public void SignUpMakePasswordVisible()
+    {
+     
+        //TODO: Research on how to make Hidden Password visible
+    }
+
+    // ------------ End --------------------------
+    
+    
+    
+    
 }
